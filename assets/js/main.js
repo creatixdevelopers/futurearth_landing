75f899059e64493fd6c5f802544fc7ac203e0cf4
First Stock <harsh.shah027@gmail.com>
2021-09-08 13:29:23 +0530

    Changes made

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,39 +14,6 @@
   }
 
   /**
-   * Easy event listener function
-   */
-  const on = (type, el, listener, all = false) => {
-    let selectEl = select(el, all)
-    if (selectEl) {
-      if (all) {
-        selectEl.forEach(e => e.addEventListener(type, listener))
-      } else {
-        selectEl.addEventListener(type, listener)
-      }
-    }
-  }
-
-  /**
-   * Easy on scroll event listener 
-   */
-  const onscroll = (el, listener) => {
-    el.addEventListener('scroll', listener)
-  }
-
-
-  /**
-   * Scroll with ofset on page load with hash links in the url
-   */
-  window.addEventListener('load', () => {
-    if (window.location.hash) {
-      if (select(window.location.hash)) {
-        scrollto(window.location.hash)
-      }
-    }
-  });
-
-  /**
    * Preloader
    */
   let preloader = select('#preloader');
